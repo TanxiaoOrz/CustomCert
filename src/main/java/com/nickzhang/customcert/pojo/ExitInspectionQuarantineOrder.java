ff5726e4947e853b48c09aae595abdd07164c25a
Tanxiao_Orz_DeskTop <13671985248@163.com>
2026-01-05 22:46:20 +0800

    定义出入检疫检验pojo相关类

---
+++ src/main/java/com/nickzhang/customcert/pojo/ExitInspectionQuarantineOrder.java
@@ -0,0 +1,16 @@
+package com.nickzhang.customcert.pojo;
+
+import com.nickzhang.customcert.annotation.Table;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2026/1/5 22:39
+ * @PackageName: com.nickzhang.customcert.pojo
+ * @ClassName: ExitInspectionQuarantineOrder
+ * @Description: 出入境检疫单
+ * @Version: 1.0
+ */
+@Table(xmlName = "EEntDeclIo", dbName = "Dcl_B_Io_Decl")
+public class ExitInspectionQuarantineOrder {
+
+}
---
+++ src/main/java/com/nickzhang/customcert/pojo/ExitInspectionQuarantineOrderGoods.java
@@ -0,0 +1,15 @@
+package com.nickzhang.customcert.pojo;
+
+import com.nickzhang.customcert.annotation.Table;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2026/1/5 22:43
+ * @PackageName: com.nickzhang.customcert.pojo
+ * @ClassName: ExitInspectionQuarantineOrderGoods
+ * @Description: 出入境检疫单商品明细
+ * @Version: 1.0
+ */
+@Table(xmlName = "ITF_DCL_IO_DECL_GOODS", dbName = "Dcl_B_Io_Decl_Goods")
+public class ExitInspectionQuarantineOrderGoods {
+}
