690baa7fd9f1475e7ea9c0c5f5d1ad52008c1b98
Tanxiao_Orz_DeskTop <13671985248@163.com>
2026-01-05 11:01:02 +0800

    海关凭证生成服务 初始化

---
+++ src/main/java/com/nickzhang/customcert/CustomCertApplication.java
@@ -0,0 +1,13 @@
+package com.nickzhang.customcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickzhang/customcert/CustomCertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickzhang.customcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
